f07ba771ca6bbc036b7e9e202f8cf268de6251d1
Ikhsan Assaat <ixnixnixn@yahoo.com>
2015-06-20 02:03:41 +0100

    Initial Commit

---
+++ OnTour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OnTour
+//
+//  Created by Ikhsan Assaat on 6/20/15.
+//  Copyright © 2015 Ikhsan Assaat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
