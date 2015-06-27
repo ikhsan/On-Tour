8fb530c1f99d669e72b4e277bcb5862913024106
Ikhsan Assaat <ixnixnixn@yahoo.com>
2015-06-27 12:15:24 +0100

    remove ignored file

--- Pods/Target Support Files/Pods-JPSThumbnailAnnotation/Pods-JPSThumbnailAnnotation-umbrella.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "JPSThumbnail.h"
-#import "JPSThumbnailAnnotation.h"
-#import "JPSThumbnailAnnotationView.h"
-
-FOUNDATION_EXPORT double JPSThumbnailAnnotationVersionNumber;
-FOUNDATION_EXPORT const unsigned char JPSThumbnailAnnotationVersionString[];
-
